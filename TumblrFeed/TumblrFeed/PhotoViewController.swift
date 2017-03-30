14b8556b652816ca3f0e47f00d55a51d152fd6fc
Sabareesh Kappagantu <sabareesh@Sabareeshs-MBP.lan>
2017-03-30 12:46:44 -0700

    Successfully added infinte scrolling and sections to the Application. Also added section headers and profile views

--- TumblrFeed/TumblrFeed/PhotoViewController.swift
+++ TumblrFeed/TumblrFeed/PhotoViewController.swift
@@ -13,13 +13,20 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     @IBOutlet weak var tumblrTableView: UITableView!
     var posts: [NSDictionary] = []
+    let headerViewIdentifier = "CellHeaderView"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tumblrTableView.insertSubview(refreshControl, at: 0)
+        
         tumblrTableView.delegate = self
         tumblrTableView.dataSource = self
         tumblrTableView.rowHeight = 240
+        
+        tumblrTableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: headerViewIdentifier)
 
         //defining the url to use for getting Tumblr Data
         let tumblrURL = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
@@ -47,7 +54,7 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
                         self.tumblrTableView.reloadData()
                     }
                 }
-        });
+        })
         task.resume()
         
         
@@ -63,7 +70,7 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
     //Implemnting TableViewController specific methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -73,7 +80,7 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
         if (posts.count != 0){
-            let post = posts[indexPath.row]
+            let post = posts[indexPath.section]
             if let photos = post.value(forKey: "photos") as? [NSDictionary] {
                 
                 let imageUrlString = photos[0].value(forKeyPath: "original_size.url") as? String
@@ -93,14 +100,81 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
         
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let header = tumblrTableView.dequeueReusableHeaderFooterView(withIdentifier: headerViewIdentifier)! as UITableViewHeaderFooterView
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).cgColor
+        profileView.layer.borderWidth = 1;
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label.textAlignment = .center
+        label.text = posts[section].value(forKey: "blog_name") as? String
+        // set the avatar
+        profileView.setImageWith(NSURL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/avatar")! as URL)
+        headerView.addSubview(profileView)
+        headerView.addSubview(label)
+        
+        header.addSubview(headerView)
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! PhotoDetailsViewController
         let indexPath = tumblrTableView.indexPath(for: sender as! UITableViewCell)!
         
-        let image = self.posts[indexPath.row]["photos"] as! [NSDictionary]
+        let image = self.posts[indexPath.section]["photos"] as! [NSDictionary]
         vc.imageUrl = image[0].value(forKeyPath: "original_size.url") as? String
         
     }
+    
+    func refreshControlAction (_ refreshControl: UIRefreshControl){
+        
+        let tumblrURL = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: tumblrURL!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task: URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.tumblrTableView.reloadData()
+                        refreshControl.endRefreshing()
+                    }
+                }
+        })
+        
+        task.resume()
+        
+    }
+    
 
     /*
     // MARK: - Navigation
